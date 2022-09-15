eb5285604d7657a440bc605d3bb58fb2725ea33a
Luke Bakken <luke@bakken.io>
2022-09-15 08:55:10 -0700

    Update with default creds

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using Amqp;
 
-var address = new Address($"amqp://myusername:{Uri.EscapeDataString("mypassword")}@myhost.com:5672");
+var address = new Address($"amqp://guest:guest@localhost:5672");
 var con = new Connection(address);
 var session1 = new Session(con);
 var session2 = new Session(con);
